7fe4361f154378ea24b1b59a5e3075cbb47f5c8f
HouardQuentin <quentin.houard@etu.minesparis.psl.eu>
2025-01-31 10:21:10 +0100

    uiuiui

--- game.hpp
+++ game.hpp
@@ -1,1 +1,17 @@
-in
+enum class TypeCase {
+    EMPTY,
+    VERTIWALL,
+    HORIWALL,
+    FLOR,
+    GATE,
+    CORRIDOR,
+    HERO
+};
+
+class Board {
+    const int nb_rows;
+    const int nb_cols;
+    std::vector<std::vector<TypeCase>> board;
+public:
+    Board(int nb_rows = 30, int nb_cols = 30) : nb_rows(nb_rows), nb_cols(nb_cols) {}
+};
--- main.cpp
+++ main.cpp
@@ -1,3 +1,12 @@
+#include <iostream>
+#include "game.hpp"
+
+/// Affichage
+const int SCREEN_WIDTH = 30;
+const int SCREEN_HEIGHT = 30;
+
+
+
 int main(){
     
     return 1;
