d78199f6739a2fc7c7d4927078a498f91921d124
Eliott-Dumas <eliott.dumas@etu.minesparis.psl.eu>
2025-01-31 16:02:42 +0100

    contre lol

---
+++ main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <string>
+#include "game.hpp"
+#include "hero.hpp"
+/// Affichage
+const int SCREEN_WIDTH = 30;
+const int SCREEN_HEIGHT = 20;
+
+
+
+int main(){
+    bool game = true;
+    char input;
+    Board board = Board(SCREEN_WIDTH, SCREEN_HEIGHT);
+    //backgroundClear();
+    Hero hero("Brandon", 3, 3);
+    board.initialize(&hero);
+    board.print_board();
+    while(game){
+        std::string message = "";
+        std::cin >> input;
+        if(input == 'p'){}
+        else if(input == 's'){
+            TypeCase _case = board.get_case(hero.y + 1, hero.x);
+            if((_case==TypeCase::GROUND)||(_case==TypeCase::GATE)||(_case==TypeCase::CORRIDOR)||(_case==TypeCase::STAIRS)){
+                hero.y++;
+            }
+            else{
+                message.append("You try to walk into a wall and hurt your nose... (-1HP)\n");
+                hero.current_hp--;
+            }
+        }
+        else if(input == 'q'){
+            TypeCase _case = board.get_case(hero.y, hero.x - 1);
+            if((_case==TypeCase::GROUND)||(_case==TypeCase::GATE)||(_case==TypeCase::CORRIDOR)||(_case==TypeCase::STAIRS)){
+                hero.x--;
+            }
+            else{
+                message.append("You try to walk into a wall and hurt your nose... (-1HP)\n");
+                hero.current_hp--;
+            }
+        }
+        else if(input == 'z'){
+            TypeCase _case = board.get_case(hero.y - 1, hero.x);
+            if((_case==TypeCase::GROUND)||(_case==TypeCase::GATE)||(_case==TypeCase::CORRIDOR)||(_case==TypeCase::STAIRS)){
+                hero.y--;
+            }
+            else{
+                message.append("You try to walk into a wall and hurt your nose... (-1HP)\n");
+                hero.current_hp--;
+            }
+        }
+        else if(input == 'd'){
+            TypeCase _case = board.get_case(hero.y, hero.x + 1);
+            if((_case==TypeCase::GROUND)||(_case==TypeCase::GATE)||(_case==TypeCase::CORRIDOR)||(_case==TypeCase::STAIRS)){
+                hero.x++;
+            }
+            else{
+                message.append("You try to walk into a wall and hurt your nose... (-1HP)\n");
+                hero.current_hp--;
+            }
+        }
+        board.deplace_hero(hero);
+        board.print_board();
+        std::printf("%s", message.c_str());
+    }
+    return EXIT_SUCCESS;
+}
