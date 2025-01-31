493946a194e5b47ab87e2bac460693632b028806
Eliott-Dumas <eliott.dumas@etu.minesparis.psl.eu>
2025-01-31 15:07:28 +0100

    movement implemented

--- keyboard_event.hpp
+++ keyboard_event.hpp
@@ -1,32 +1,32 @@
-#pragma once
-#include <iostream>
-// #include <sys/ioctl.h>
-// #include <termios.h>
-#include <cstdio>
-#include <ctime>
+// #pragma once
+// #include <iostream>
+// // #include <sys/ioctl.h>
+// // #include <termios.h>
+// #include <cstdio>
+// #include <ctime>
 
-static const int STDIN = 0;
-static bool initialized = false;
+// static const int STDIN = 0;
+// static bool initialized = false;
 
-void backgroundClear() {
-  int out = system("clear");
-  if (out != 0) {
-    std::cerr << "clear command failed" << std::endl;
-    exit(3);
-  }
-}
-
-
-// int keyEvent(){
-//     if(!initialized){
-//         termios term;
-//         tcgetattr(STDIN, &term);
-//         term.c_lflag &= ~ICANON;
-//         tcsetattr(STDIN, TCSANOW, &term);
-//         setbuf(stdin, NULL);
-//         initialized = true;
-//     }
-//     int bytesWaiting = 0;
-//     while(!bytesWaiting && ioctl(STDIN, FIONREAD, &bytesWaiting) >= 0){}
-//     return bytesWaiting;
+// void backgroundClear() {
+//   int out = system("clear");
+//   if (out != 0) {
+//     std::cerr << "clear command failed" << std::endl;
+//     exit(3);
+//   }
 // }
+
+
+// // int keyEvent(){
+// //     if(!initialized){
+// //         termios term;
+// //         tcgetattr(STDIN, &term);
+// //         term.c_lflag &= ~ICANON;
+// //         tcsetattr(STDIN, TCSANOW, &term);
+// //         setbuf(stdin, NULL);
+// //         initialized = true;
+// //     }
+// //     int bytesWaiting = 0;
+// //     while(!bytesWaiting && ioctl(STDIN, FIONREAD, &bytesWaiting) >= 0){}
+// //     return bytesWaiting;
+// // }
