59f26b49aee7fe305b99dadfc435f4712fe81608
Gabriel Vandersippe <gabriel.vandersippe@etu.minesparis.psl.eu>
2025-01-31 10:21:18 +0100

    Add Hero and Objects basic structure

---
+++ hero.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#include "objects.hpp"
+
+class Inventory {
+	
+};
+
+class Hero {
+
+	char symbol = '@';
+
+	int max_hp;
+	int current_hp;
+
+	Inventory* i;
+};
---
+++ objects.hpp
@@ -0,0 +1,14 @@
+#include <string>
+using namespace std;
+
+
+struct Object {
+
+	char symbol;
+	string description;
+
+};
+
+struct Potion : public Object {
+	//Choses
+};
